2e5ef55f36615e0e253d43aa8e3a87e54f08bc0b
arehal <arehal@sfu.ca>
2019-01-18 14:28:29 -0800

    created header and source files for WorldHandler class

---
+++ lib/model/include/WorldHandler.h
@@ -0,0 +1,16 @@
+//
+// Created by arehal on 1/18/19.
+//
+
+#ifndef WEBSOCKETNETWORKING_WORLDHANDLER_H
+#define WEBSOCKETNETWORKING_WORLDHANDLER_H
+
+namespace model{
+    class WorldHandler{
+    public:
+        WorldHandler();
+
+    };
+}
+
+#endif //WEBSOCKETNETWORKING_WORLDHANDLER_H
---
+++ lib/model/src/WorldHandler.cpp
@@ -0,0 +1,12 @@
+//
+// Created by arehal on 1/18/19.
+//
+
+#include "WorldHandler.h"
+
+using model::WorldHandler;
+
+namespace model {
+
+    WorldHandler::WorldHandler(){}
+}
