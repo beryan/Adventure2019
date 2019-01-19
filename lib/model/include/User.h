e2225abdaf13f74fd3927169240f622f6cf7d565
swanhell <swanhell@sfu.ca>
2019-01-18 19:15:45 -0800

    remove Race enum in User header

--- lib/model/include/User.h
+++ lib/model/include/User.h
@@ -14,33 +14,30 @@ static const double MAX_HEALTH = 100.00;
 static const int MAX_KEYWORDS = 8;
 
 namespace model {
-    //TODO: Change values to more appropriate values
-    enum class Race {Human, Weeb, Furries};
-
     class User {
     public:
         inline User(int id) : id(id), health(STARTING_HEALTH), description({}) {}
 
         inline User(int id, std::vector<std::string> keys, std::vector<std::string> desc) :
-            id(id), keywords(keys), description(desc) {}
+                id(id), keywords(keys), description(desc) {}
 
-        inline int getId() {return id;};
+        inline int getId() { return id; };
 
-        inline void setId(int id) {this->id = id;};
+        inline void setId(int id) { this->id = id; };
 
-        inline void addKeyword(std::string key) {keywords.push_back(std::move(key));};
+        inline void addKeyword(std::string key) { keywords.push_back(std::move(key)); };
 
-        inline void removeKeyword(int index) {keywords.erase(keywords.begin() + index);};
+        inline void removeKeyword(int index) { keywords.erase(keywords.begin() + index); };
 
-        inline std::vector<std::string> getKeywords() {return keywords;};
+        inline std::vector<std::string> getKeywords() { return keywords; };
 
-        inline double getHealth() {return health;};
+        inline double getHealth() { return health; };
 
-        inline void setHealth(double health) {this->health = health;};
+        inline void setHealth(double health) { this->health = health; };
 
-        inline std::vector<std::string> getDescription() {return this->description;};
+        inline std::vector<std::string> getDescription() { return this->description; };
 
-        inline void setDescription(std::string desc) {description.push_back(std::move(desc));};
+        inline void setDescription(std::string desc) { description.push_back(std::move(desc)); };
 
     private:
         int id;
@@ -52,8 +49,9 @@ namespace model {
         std::vector<std::string> description;
     };
 
-    inline double getMaxHealth() {return MAX_HEALTH;};
-    inline double getStartingHealth() {return STARTING_HEALTH;};
+    inline double getMaxHealth() { return MAX_HEALTH; };
+
+    inline double getStartingHealth() { return STARTING_HEALTH; };
 }
 
 #endif //WEBSOCKETNETWORKING_USER_H
