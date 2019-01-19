9b1a41a18ca8cf2215370ee38535a1ca765c35e2
Brittany Ryan <beryan@sfu.com>
2019-01-18 21:05:01 -0800

    Add Door class

---
+++ lib/model/src/Door.cpp
@@ -0,0 +1,91 @@
+/*
+* Door.cpp
+*
+* Class Description: A class designed to represent exits from a room.
+*
+* Created on: January 17, 2019
+* Author: Brittany Ryan, 301217765
+*/
+
+#include "Door.h"
+#include <iostream>
+
+using model::Door;
+
+namespace model {
+
+    //constructors
+    Door::Door() {
+    	dir = "";
+    	desc = {};
+    	keywords = {};
+    	to = -1;
+    }
+
+    Door::Door(std::string d, int t) {
+    	dir = d;
+    	desc = {};
+    	keywords = {};
+    	to = t;
+    }
+
+    Door::Door(std::string direction, std::vector<std::string> description, std::vector<std::string> k, int t) {
+    	dir = direction;
+    	desc = description;
+    	keywords = k;
+    	to = t;
+    }
+
+    //getters and setters
+    std::string Door::getDir() {
+    	return dir;
+    }
+
+    std::vector<std::string> Door::getDesc() {
+    	return desc;
+    }
+
+    std::vector<std::string> Door::getKeywords() {
+    	return keywords;
+    }
+
+    int Door::getTo() {
+    	return to;
+    }
+
+    void Door::setDir(std::string d) {
+    	dir = d;
+    }
+
+    void Door::setDesc(std::vector<std::string> d) {
+    	desc = d;
+    }
+
+    void Door::setKeywords(std::vector<std::string> k) {
+    	keywords = k;
+    }
+
+    void Door::setTo(int t) {
+    	to = t;
+    }
+
+    //print object
+    std::ostream& operator<<(std::ostream& os, const Door& rhs) {
+    	os << "Direction: " + rhs.dir << std::endl;
+
+    	os << "Description: \n";
+    	for (std::string s : rhs.desc) {
+    		os << s << std::endl;
+    	}
+
+    	os << "Keywords: \n";
+    	for (std::string s : rhs.keywords) {
+    		os << s << std::endl;
+    	}
+
+    	os << "Connects to room: " << rhs.to << std::endl;
+
+    	return os;
+    }
+
+}
