79ed7deb863ac06cb11e1257dbf850a0ee175fbf
arehal <arehal@sfu.ca>
2019-01-19 16:42:24 -0800

    added methods to insert and print Users

--- lib/model/src/World.cpp
+++ lib/model/src/World.cpp
@@ -2,10 +2,12 @@
 // Created by arehal on 1/17/19.
 //
 
+#include <iostream>
+
 #include "World.h"
-#include "User.h"
 
 using model::World;
+using model::Player;
 
 namespace model {
 
@@ -15,7 +17,19 @@ namespace model {
         return this->usersMap;
     }
 
+    void World::insertUser(Player player){
+        usersMap.insert(std::pair<int,Player>(player.getId(), player));
+    }
+
 //    std::map<int, Area> World::getAreaMap(){
 //        return this->areasMap;
 //    }
+
+    void World::printUsers() {
+        std::cout << "Users:" << std::endl;
+
+        for (auto& [key, value]: usersMap) {
+            std::cout << value.getId() << ". " <<  value.getUsername() << std::endl;
+        }
+    }
 };
