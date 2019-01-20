31d12cd970a01bf35a7bf04b3e9d66cbe86f8133
arehal <arehal@sfu.ca>
2019-01-19 16:35:08 -0800

    added methods to add and remove Users and Areas

--- lib/model/include/WorldHandler.h
+++ lib/model/include/WorldHandler.h
@@ -5,11 +5,18 @@
 #ifndef WEBSOCKETNETWORKING_WORLDHANDLER_H
 #define WEBSOCKETNETWORKING_WORLDHANDLER_H
 
+#include "Player.h"
+#include "World.h"
 namespace model{
     class WorldHandler{
     public:
         WorldHandler();
 
+        void addUser(Player player);
+        void removeUser(Player player);
+
+//        void addArea(Area area);
+//        void removeArea(Area area);
     };
 }
 
--- lib/model/src/WorldHandler.cpp
+++ lib/model/src/WorldHandler.cpp
@@ -8,5 +8,25 @@ using model::WorldHandler;
 
 namespace model {
 
-    WorldHandler::WorldHandler(){}
+    WorldHandler::WorldHandler(){
+        std::cout << "WorldHandler Created" << std::endl;
+        parseJSON();
+    }
+
+    void WorldHandler::addUser(Player player){
+        std::cout << "Adding " << player.getId() << " " << player.getUsername() << " to users" << std::endl;
+        this->world.insertUser(player);
+    }
+
+    void WorldHandler::removeUser(Player player){
+        this->world.getUserMap().erase(player.getId());
+    }
+
+//    void WorldHandler::addArea(Area area){
+//        this->world.getAreaMap().insert(std::make_pair(area.getId(), area));
+//    }
+//
+//    void WorldHandler::removeArea(Area area){
+//        this->world.getAreaMap().erase(area.getId());
+//    }
 }
