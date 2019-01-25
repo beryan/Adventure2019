be7c0139a46384630c66230e47ed399802a44b72
Junho <junhok@sfu.ca>
2019-01-25 14:27:31 -0800

    setup initial test files for the project
    - Simple tests added to character-test.cpp
    - Simple test added to player-test.cpp

--- testing/character-test.cpp
+++ testing/character-test.cpp
@@ -10,10 +10,21 @@ using model::Role;
 using model::Character;
 
 namespace {
-    TEST(CharacterTestSuite, shouldReturnCorrectCharacterId) {
+    TEST(CharacterTestSuite, canConstructCharacter) {
         int expected_id = 12345;
         Character character{expected_id};
 
         EXPECT_EQ(character.getId(), expected_id);
+    }
+
+    TEST(CharacterTestSuite, canReturnCorrectHealthAfterReducing) {
+        int damage = 30.0;
+        int expected_id = 12345;
+        double expected_health = Character::STARTING_HEALTH - damage;
+
+        Character character{expected_id};
+        character.reduceHealth(damage);
+
+        EXPECT_EQ(character.getHealth(), expected_health);
     }
 }
