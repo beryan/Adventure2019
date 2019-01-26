79031cd7bc77bb716d2a828c1eb60cf53c3b51ca
waswaolunga <olungaw@gmail.com>
2019-01-25 19:21:58 -0800

    Revert Door.h

--- lib/model/include/Door.h
+++ lib/model/include/Door.h
@@ -7,67 +7,41 @@
 * Author: Brittany Ryan, 301217765
 */
 
-#ifndef DOOR_H
-#define DOOR_H
+#ifndef WEBSOCKETNETWORKING_DOOR_H
+#define WEBSOCKETNETWORKING_DOOR_H
 
 #include <string>
 #include <vector>
 
 namespace model {
 
-	/**
-    *  @class Door
-    *
-    *  @brief A class describing a door.
-    *
-    *  The Door class represents an exit from a room along
-    *  with information regarding which adjacent room the door leads to
-    *  and how to get to this adjacent room.
-    */
+	class Door {
 
-    class Door {
-
-	public:
-		Door();
-
-		Door(std::string direction, int idOfRoomDoorLeadsTo);
-
-		Door(
-			std::string direction,
-			std::vector<std::string> description,
-			std::vector<std::string> keywords,
-			int idOfRoomDoorLeadsTo
-		);
-
-		std::string getDirection() const;
-
-		void setDirection(std::string direction);
-
-		std::vector<std::string> getDescriptions() const;
-
-		void setDescriptions(std::vector<std::string> descriptions);
-
-		std::vector<std::string> getKeywords() const;
-
-		void setKeywords(std::vector<std::string> keywords);
-
-		int getIdOfRoomDoorLeadsTo() const;
-
-		void setIdOfRoomDoorLeadsTo(int idOfRoomDoorLeadsTo);
+	private:
+		std::string dir; //direction
+		std::vector<std::string> desc;
+		std::vector<std::string> keywords;
+		int to; //id of room door leads to
 
 		friend std::ostream& operator<<(std::ostream& os, const Door& rhs);
 
-		static constexpr int DEFAULT_ID_OF_ROOM_DOOR_LEADS_TO = -1;
+	public:
+		//constructors
+		Door();
+		Door(std::string dir, int to);
+		Door(std::string dir, std::vector<std::string> desc, std::vector<std::string> keywords, int to);
 
-	private:
-      	std::string direction;
+		//getters and setters
+		std::string getDir();
+		std::vector<std::string> getDesc();
+		std::vector<std::string> getKeywords();
+		int getTo();
+		void setDir(std::string dir);
+		void setDesc(std::vector<std::string> desc);
+		void setKeywords(std::vector<std::string> keywords);
+		void setTo(int to);
 
-      	std::vector<std::string> description;
-
-      	std::vector<std::string> keywords;
-
-      	int idOfRoomDoorLeadsTo;
-    };
+	};
 }
 
-#endif // DOOR_H
+#endif
