323476d11f720963885b6531db778bcb3ef66dfa
swanhell <stephen_wanhella@sfu.ca>
2019-02-06 16:56:40 -0800

    write tests for Object class

--- testing/object-test.cpp
+++ testing/object-test.cpp
@@ -17,4 +17,70 @@ namespace {
         EXPECT_EQ(model::Slot::Misc, myObject.getSlot());
         EXPECT_EQ(myExtraInfo, myObject.getExtraObjectInfo());
     }
+
+    TEST(ObjectTestSuite, canUseConstructor) {
+        model::ID myId = 42069;
+        std::string name = "Skateboard";
+        std::string shortDesc = "This is a magical skateboard";
+        std::vector<std::string> longDesc = {"my", "long", "description", "?"};
+        std::vector<std::string> keywords = {"skateboard", "travel", "fast", "cool"};
+        model::Slot mySlot = model::Slot::Head;
+        model::Object myObject{myId, name, shortDesc, longDesc, keywords, mySlot};
+
+        model::ExtraObjectInfo emptyExtraInfo;
+
+        EXPECT_EQ(myId, myObject.getId());
+        EXPECT_EQ(name, myObject.getName());
+        EXPECT_EQ(shortDesc, myObject.getShortDescription());
+        EXPECT_EQ(longDesc, myObject.getLongDescription());
+        EXPECT_EQ(keywords, myObject.getKeywords());
+        EXPECT_EQ(mySlot, myObject.getSlot());
+        EXPECT_EQ(emptyExtraInfo, myObject.getExtraObjectInfo());
+    }
+
+    TEST(ObjectTestSuite, canUseExtraObjectInfoConstructor) {
+        model::ID myId = 42069;
+        std::string name = "Skateboard";
+        std::string shortDesc = "This is a magical skateboard";
+        std::vector<std::string> longDesc = {"my", "long", "description", "?"};
+        std::vector<std::string> keywords = {"skateboard", "travel", "fast", "cool"};
+        model::Slot mySlot = model::Slot::Head;
+        model::ExtraObjectInfo myExtraInfo{{"asdf"}, {"hello"}};
+        model::Object myObject{myId, name, shortDesc, longDesc, keywords, mySlot, myExtraInfo};
+
+        EXPECT_EQ(myId, myObject.getId());
+        EXPECT_EQ(name, myObject.getName());
+        EXPECT_EQ(shortDesc, myObject.getShortDescription());
+        EXPECT_EQ(longDesc, myObject.getLongDescription());
+        EXPECT_EQ(keywords, myObject.getKeywords());
+        EXPECT_EQ(mySlot, myObject.getSlot());
+        EXPECT_EQ(myExtraInfo, myObject.getExtraObjectInfo());
+    }
+
+    TEST(ObjectTestSuite, canMakeObjectFromSetters) {
+        model::ID myId = 42069;
+        std::string name = "Skateboard";
+        std::string shortDesc = "This is a magical skateboard";
+        std::vector<std::string> longDesc = {"my", "long", "description", "?"};
+        std::vector<std::string> keywords = {"skateboard", "travel", "fast", "cool"};
+        model::Slot mySlot = model::Slot::Head;
+        model::ExtraObjectInfo myExtraInfo{{"asdf"}, {"hello"}};
+        model::Object myObject{};
+
+        myObject.setId(myId);
+        myObject.setName(name);
+        myObject.setShortDescription(shortDesc);
+        myObject.setLongDescription(longDesc);
+        myObject.setKeywords(keywords);
+        myObject.setSlot(mySlot);
+        myObject.setExtraObjectInfo(myExtraInfo);
+
+        EXPECT_EQ(myId, myObject.getId());
+        EXPECT_EQ(name, myObject.getName());
+        EXPECT_EQ(shortDesc, myObject.getShortDescription());
+        EXPECT_EQ(longDesc, myObject.getLongDescription());
+        EXPECT_EQ(keywords, myObject.getKeywords());
+        EXPECT_EQ(mySlot, myObject.getSlot());
+        EXPECT_EQ(myExtraInfo, myObject.getExtraObjectInfo());
+    }
 }
