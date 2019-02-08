8cb79051ab703f35906238e485b66639933d8754
arehal <arehal@sfu.ca>
2019-02-08 00:16:18 -0800

    created functions to add NPCs and Objects to Rooms

--- lib/middleware/src/DataManager.cpp
+++ lib/middleware/src/DataManager.cpp
@@ -24,6 +24,47 @@ namespace DataManager {
 
     namespace {
 
+        void addNPCsToRooms(std::vector<Room>& rooms, std::vector<NPC>& npcs, const std::vector<Reset>& resets) {
+            for(Reset r : resets) {
+                if(r.getAction() == "npc") {
+                    auto roomID = r.getRoom();
+                    auto room = std::find_if(rooms.begin(), rooms.end(),
+                                             [roomID](const Room & room) -> bool { return room.getId() == roomID ; });
+
+                    auto npcID = r.getId();
+                    auto npc = std::find_if(npcs.begin(), npcs.end(),
+                                            [npcID](const NPC & n) -> bool { return n.getId() == npcID ; });
+
+                    if(room != rooms.end() && npc != npcs.end()) {
+                        room->addNPC(*npc);
+                        std::cout << "NPC " << npc->getId() << " added to Room " << room->getId() << std::endl;
+                    }
+
+                }
+            }
+        }
+
+        void addObjectsToRooms(std::vector<Room>& rooms, std::vector<Object>& objects, const std::vector<Reset>& resets) {
+            for(Reset r : resets) {
+                if(r.getAction() == "object") {
+                    auto roomID = r.getRoom();
+                    auto room = std::find_if(rooms.begin(), rooms.end(),
+                                             [roomID](const Room & room) -> bool { return room.getId() == roomID ; });
+
+                    auto objectID = r.getId();
+                    auto object = std::find_if(objects.begin(), objects.end(),
+                                               [objectID](const Object & o) -> bool { return o.getId() == objectID ; });
+
+                    if(room != rooms.end() && object != objects.end()) {
+                        room->addObject(*object);
+                        std::cout << "Object " << object->getId() << " added to Room " << room->getId() << std::endl;
+                    }
+
+                }
+            }
+        }
+
+
         void parseDataJson(const std::string& filePath) {
 
             std::ifstream ifs(filePath);
@@ -51,10 +92,12 @@ namespace DataManager {
 
             if(t.find("NPCS") != t.end()){
                 npcs = t.at("NPCS").get<std::vector<NPC>>();
+                addNPCsToRooms(rooms, npcs, resets);
             }
 
             if(t.find("OBJECTS") != t.end()){
                 objects = t.at("OBJECTS").get<std::vector<Object>>();
+                addObjectsToRooms(rooms, objects, resets);
             }
 
             if(t.find("HELPS") != t.end()){
