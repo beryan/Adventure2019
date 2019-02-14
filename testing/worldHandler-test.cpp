8dd0366be0aa103e627f20ed95c765465cddb9ae
swanhell <stephen_wanhella@sfu.ca>
2019-02-14 11:34:34 -0800

    Start WorldHandler tests

---
+++ testing/worldHandler-test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Stephen Wanhella on 2019-02-14.
+//
+
+#include <WorldHandler.h>
+#include "gtest/gtest.h"
+#include "Character.h"
+
+namespace {
+    TEST(WorldHandlerTestSuite, canMakeWorld) {
+        model::WorldHandler worldHandler;
+    }
+}
