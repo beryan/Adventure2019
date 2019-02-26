f63e429c2157aa4e8cb04555f6ea2867b58bd199
Junho <junhok@sfu.ca>
2019-02-26 15:42:23 -0800

    Document Equipment

--- lib/model/include/Equipment.h
+++ lib/model/include/Equipment.h
@@ -20,20 +20,55 @@ namespace model {
     public:
         Equipment();
 
+        /**
+         * @return the immutable unordered map of Equipment
+         */
         std::unordered_map<int, Object> getMappedEquipment();
 
+        /**
+         * @return the immutable vector form of Equipment
+         */
         std::vector<Object> getVectorEquipment() const;
 
+        /**
+         * Maps the list of items into Equipment
+         * @param items: Items to be mapped
+         */
         void mapEquipment(std::vector<Object> items);
 
+        /**
+         * Equips an item
+         * @param object: Item to be equipped
+         */
         void equipItem(const Object &object);
 
+        /**
+         * Uneqiups an item
+         * @param item: Item to be unequipped
+         * @return Ownership of the item that was unequipped
+         */
         Object unequipItem(const Object &item);
 
+        /**
+         * Unequips a specific slot in Equipment
+         * @param slot: Slot to be unequipped
+         * @return Returns the ownership of the item that existed in the slot that was
+         * unequipped
+         */
         Object unequipSlot(const Slot &slot);
 
+        /**
+         * Checks to see if the Slot is currently occupied by an item
+         * @param slot: Slot to be checked
+         * @return True if an item exists in the slot, False if not
+         */
         bool isSlotOccupied(const Slot &slot);
 
+        /**
+         * Checks to see if a specific item is equipped
+         * @param item: Item to be checked
+         * @return Returns True if the item is currently equipped, False if not
+         */
         bool isItemEquipped(const Object &item);
     };
 }
