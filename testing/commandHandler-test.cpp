b789fca9ddfce5ed0784143e8ef5ccc3c3b0144d
swanhell <swanhell@sfu.ca>
2019-02-25 17:25:19 -0800

    add 'using' statements to command handler tests

--- testing/commandHandler-test.cpp
+++ testing/commandHandler-test.cpp
@@ -6,84 +6,87 @@
 #include "gmock/gmock.h"
 #include "CommandHandler.h"
 
+using model::Command;
+using model::CommandHandler;
+
 namespace {
     TEST(CommandHandlerTestSuite, canGetCommandsFromStrings) {
-        model::CommandHandler commandHandler;
-        EXPECT_EQ(model::Command::Help, commandHandler.getDefaultCommand("help"));
-        EXPECT_EQ(model::Command::Login, commandHandler.getDefaultCommand("login"));
-        EXPECT_EQ(model::Command::Logout, commandHandler.getDefaultCommand("logout"));
-        EXPECT_EQ(model::Command::Look, commandHandler.getDefaultCommand("look"));
-        EXPECT_EQ(model::Command::Move, commandHandler.getDefaultCommand("move"));
-        EXPECT_EQ(model::Command::Quit, commandHandler.getDefaultCommand("quit"));
-        EXPECT_EQ(model::Command::Register, commandHandler.getDefaultCommand("register"));
-        EXPECT_EQ(model::Command::Say, commandHandler.getDefaultCommand("say"));
-        EXPECT_EQ(model::Command::Shutdown, commandHandler.getDefaultCommand("shutdown"));
-        EXPECT_EQ(model::Command::Tell, commandHandler.getDefaultCommand("tell"));
-        EXPECT_EQ(model::Command::Yell, commandHandler.getDefaultCommand("yell"));
+        CommandHandler commandHandler;
+        EXPECT_EQ(Command::Help, commandHandler.getDefaultCommand("help"));
+        EXPECT_EQ(Command::Login, commandHandler.getDefaultCommand("login"));
+        EXPECT_EQ(Command::Logout, commandHandler.getDefaultCommand("logout"));
+        EXPECT_EQ(Command::Look, commandHandler.getDefaultCommand("look"));
+        EXPECT_EQ(Command::Move, commandHandler.getDefaultCommand("move"));
+        EXPECT_EQ(Command::Quit, commandHandler.getDefaultCommand("quit"));
+        EXPECT_EQ(Command::Register, commandHandler.getDefaultCommand("register"));
+        EXPECT_EQ(Command::Say, commandHandler.getDefaultCommand("say"));
+        EXPECT_EQ(Command::Shutdown, commandHandler.getDefaultCommand("shutdown"));
+        EXPECT_EQ(Command::Tell, commandHandler.getDefaultCommand("tell"));
+        EXPECT_EQ(Command::Yell, commandHandler.getDefaultCommand("yell"));
     }
 
     TEST(CommandHandlerTestSuite, canGetInvalidCommand) {
-        model::CommandHandler commandHandler;
-        EXPECT_EQ(model::Command::InvalidCommand, commandHandler.getDefaultCommand("asdfasdfasdf"));
+        CommandHandler commandHandler;
+        EXPECT_EQ(Command::InvalidCommand, commandHandler.getDefaultCommand("asdfasdfasdf"));
     }
 
     TEST(CommandHandlerTestSuite, canGetAliasedCommands) {
-        model::CommandHandler commandHandler;
+        CommandHandler commandHandler;
         std::string testUser = "testuser";
-        EXPECT_EQ(model::Command::Help, commandHandler.getCommandForUser("test_help", testUser));
-        EXPECT_EQ(model::Command::Login, commandHandler.getCommandForUser("test_login", testUser));
-        EXPECT_EQ(model::Command::Logout, commandHandler.getCommandForUser("test_logout", testUser));
-        EXPECT_EQ(model::Command::Look, commandHandler.getCommandForUser("test_look", testUser));
-        EXPECT_EQ(model::Command::Move, commandHandler.getCommandForUser("test_move", testUser));
-        EXPECT_EQ(model::Command::Quit, commandHandler.getCommandForUser("test_quit", testUser));
-        EXPECT_EQ(model::Command::Register, commandHandler.getCommandForUser("test_register", testUser));
-        EXPECT_EQ(model::Command::Say, commandHandler.getCommandForUser("test_say", testUser));
-        EXPECT_EQ(model::Command::Shutdown, commandHandler.getCommandForUser("test_shutdown", testUser));
-        EXPECT_EQ(model::Command::Tell, commandHandler.getCommandForUser("test_tell", testUser));
-        EXPECT_EQ(model::Command::Yell, commandHandler.getCommandForUser("test_yell", testUser));
+        EXPECT_EQ(Command::Help, commandHandler.getCommandForUser("test_help", testUser));
+        EXPECT_EQ(Command::Login, commandHandler.getCommandForUser("test_login", testUser));
+        EXPECT_EQ(Command::Logout, commandHandler.getCommandForUser("test_logout", testUser));
+        EXPECT_EQ(Command::Look, commandHandler.getCommandForUser("test_look", testUser));
+        EXPECT_EQ(Command::Move, commandHandler.getCommandForUser("test_move", testUser));
+        EXPECT_EQ(Command::Quit, commandHandler.getCommandForUser("test_quit", testUser));
+        EXPECT_EQ(Command::Register, commandHandler.getCommandForUser("test_register", testUser));
+        EXPECT_EQ(Command::Say, commandHandler.getCommandForUser("test_say", testUser));
+        EXPECT_EQ(Command::Shutdown, commandHandler.getCommandForUser("test_shutdown", testUser));
+        EXPECT_EQ(Command::Tell, commandHandler.getCommandForUser("test_tell", testUser));
+        EXPECT_EQ(Command::Yell, commandHandler.getCommandForUser("test_yell", testUser));
     }
 
     TEST(CommandHandlerTestSuite, aliasedCommandsFallBackToDefault) {
-        model::CommandHandler commandHandler;
+        CommandHandler commandHandler;
         std::string testUser = "testuser_no_aliases";
-        EXPECT_EQ(model::Command::Help, commandHandler.getCommandForUser("help", testUser));
-        EXPECT_EQ(model::Command::Login, commandHandler.getCommandForUser("login", testUser));
-        EXPECT_EQ(model::Command::Logout, commandHandler.getCommandForUser("logout", testUser));
-        EXPECT_EQ(model::Command::Look, commandHandler.getCommandForUser("look", testUser));
-        EXPECT_EQ(model::Command::Move, commandHandler.getCommandForUser("move", testUser));
-        EXPECT_EQ(model::Command::Quit, commandHandler.getCommandForUser("quit", testUser));
-        EXPECT_EQ(model::Command::Register, commandHandler.getCommandForUser("register", testUser));
-        EXPECT_EQ(model::Command::Say, commandHandler.getCommandForUser("say", testUser));
-        EXPECT_EQ(model::Command::Shutdown, commandHandler.getCommandForUser("shutdown", testUser));
-        EXPECT_EQ(model::Command::Tell, commandHandler.getCommandForUser("tell", testUser));
-        EXPECT_EQ(model::Command::Yell, commandHandler.getCommandForUser("yell", testUser));
+        EXPECT_EQ(Command::Help, commandHandler.getCommandForUser("help", testUser));
+        EXPECT_EQ(Command::Login, commandHandler.getCommandForUser("login", testUser));
+        EXPECT_EQ(Command::Logout, commandHandler.getCommandForUser("logout", testUser));
+        EXPECT_EQ(Command::Look, commandHandler.getCommandForUser("look", testUser));
+        EXPECT_EQ(Command::Move, commandHandler.getCommandForUser("move", testUser));
+        EXPECT_EQ(Command::Quit, commandHandler.getCommandForUser("quit", testUser));
+        EXPECT_EQ(Command::Register, commandHandler.getCommandForUser("register", testUser));
+        EXPECT_EQ(Command::Say, commandHandler.getCommandForUser("say", testUser));
+        EXPECT_EQ(Command::Shutdown, commandHandler.getCommandForUser("shutdown", testUser));
+        EXPECT_EQ(Command::Tell, commandHandler.getCommandForUser("tell", testUser));
+        EXPECT_EQ(Command::Yell, commandHandler.getCommandForUser("yell", testUser));
     }
 
     TEST(CommandHandlerTestSuite, canMakeAndDeleteGlobalAliases) {
-        model::CommandHandler commandHandler;
+        CommandHandler commandHandler;
         std::string testUser = "testuser";
         std::string testUser_noAliases = "asdf_user";
         std::string global_help_alias = "hAlP";
 
-        commandHandler.setGlobalAlias(model::Command::Help, global_help_alias);
-        EXPECT_EQ(model::Command::Help, commandHandler.getCommandForUser("test_help", testUser));
-        EXPECT_EQ(model::Command::Help, commandHandler.getCommandForUser(global_help_alias, testUser_noAliases));
-        EXPECT_EQ(model::Command::Help, commandHandler.getCommandForUser("help", testUser_noAliases));
-        commandHandler.clearGlobalAlias(model::Command::Help);
-        EXPECT_EQ(model::Command::InvalidCommand, commandHandler.getCommandForUser(global_help_alias, testUser_noAliases));
+        commandHandler.setGlobalAlias(Command::Help, global_help_alias);
+        EXPECT_EQ(Command::Help, commandHandler.getCommandForUser("test_help", testUser));
+        EXPECT_EQ(Command::Help, commandHandler.getCommandForUser(global_help_alias, testUser_noAliases));
+        EXPECT_EQ(Command::Help, commandHandler.getCommandForUser("help", testUser_noAliases));
+        commandHandler.clearGlobalAlias(Command::Help);
+        EXPECT_EQ(Command::InvalidCommand, commandHandler.getCommandForUser(global_help_alias, testUser_noAliases));
     }
 
     TEST(CommandHandlerTestSuite, canMakeAndDeleteUserAliases) {
-        model::CommandHandler commandHandler;
+        CommandHandler commandHandler;
         std::string testUser = "new_test_user";
         std::string testUser_noAliases = "asdf_user";
         std::string user_help_alias = "hAlP";
 
-        commandHandler.setUserAlias(model::Command::Help, user_help_alias, testUser);
-        EXPECT_EQ(model::Command::Help, commandHandler.getCommandForUser(user_help_alias, testUser));
-        EXPECT_NE(model::Command::Help, commandHandler.getCommandForUser(user_help_alias, testUser_noAliases));
-        EXPECT_EQ(model::Command::Help, commandHandler.getCommandForUser("help", testUser));
-        commandHandler.clearUserAlias(model::Command::Help, testUser);
-        EXPECT_EQ(model::Command::InvalidCommand, commandHandler.getCommandForUser(user_help_alias, testUser));
+        commandHandler.setUserAlias(Command::Help, user_help_alias, testUser);
+        EXPECT_EQ(Command::Help, commandHandler.getCommandForUser(user_help_alias, testUser));
+        EXPECT_NE(Command::Help, commandHandler.getCommandForUser(user_help_alias, testUser_noAliases));
+        EXPECT_EQ(Command::Help, commandHandler.getCommandForUser("help", testUser));
+        commandHandler.clearUserAlias(Command::Help, testUser);
+        EXPECT_EQ(Command::InvalidCommand, commandHandler.getCommandForUser(user_help_alias, testUser));
     }
 }
