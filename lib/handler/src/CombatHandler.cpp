0473b5bfe5aea2c0014d1666fa9b9db439c827a7
swanhell <stephen_wanhella@sfu.ca>
2019-03-12 22:08:25 -0700

    fix clang-tidy warnings in CombatHandler

--- lib/handler/src/CombatHandler.cpp
+++ lib/handler/src/CombatHandler.cpp
@@ -16,7 +16,7 @@ namespace action {
         CombatState combat{attacker.getId(), defender.getId()};
 
         if (logic.canEnterCombat(combat)) {
-            active_combats.push_back(std::move(combat));
+            active_combats.push_back(combat);
         }
     }
 
@@ -24,12 +24,11 @@ namespace action {
         CombatState combat{attacker.getId(), defender.getId()};
 
         if (logic.canExitCombat(combat)) {
-            active_combats.erase(
-                    std::remove_if(
-                            active_combats.begin(),
-                            active_combats.end(),
-                            [&combat](const CombatState &state) {return combat == state;})
-                            );
+            auto it = std::find(active_combats.begin(), active_combats.end(), combat);
+
+            if (it != active_combats.end()) {
+                active_combats.erase(it);
+            }
         }
     }
 
