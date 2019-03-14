83afd5db8c770aa571f38cf51826644f489b8e56
Junho <junhok@sfu.ca>
2019-03-13 18:42:46 -0700

    Add documentation to CombatHandler and CombatLogic

--- lib/handler/include/CombatHandler.h
+++ lib/handler/include/CombatHandler.h
@@ -23,12 +23,33 @@ namespace action {
     public:
         CombatHandler();
 
+        /**
+         *
+         * @param attacker that will enter Combat
+         * @param defender that will enter Combat
+         */
         void enterCombat(const Character &attacker, const Character &defender);
 
+        /**
+         * The arguments are attacker and defender for name consistency sake,
+         * it doesn't matter in which order the Characters are passed in
+         * @param attacker: First character that is part of the Combat
+         * @param defender: Second character that is part of the Combat
+         */
         void exitCombat(Character &attacker, Character &defender);
 
+        /**
+         *
+         * @param attacker is the person dealing damage
+         * @param defender is the perosn being dealt the damage
+         */
         void attack(Character &attacker, Character &defender);
 
+        /**
+         *
+         * @param healer that will use their ability to heal someone
+         * @param target is the person being healed
+         */
         void heal(Character &healer, Character &target);
     };
 }
--- lib/logic/include/CombatLogic.h
+++ lib/logic/include/CombatLogic.h
@@ -21,14 +21,37 @@ namespace logic {
     public:
         CombatLogic(std::vector<CombatState> &active_combats);
 
+        /**
+         * @param combat state to be checked in active_combats
+         * @return False if state already exists between two characters in the
+         * Combat param, True if not.
+         */
         bool canEnterCombat(const CombatState &combat);
 
+        /**
+         *
+         * @param combat state to be checked in active_combats
+         * @return True if the state already exists between two characters in the Combat
+         * param, False if not.
+         */
         bool canExitCombat(const CombatState &combat);
 
+        /**
+         *
+         * @param healer that will use the ability
+         * @param target that the heal will affect
+         * @return Returns True if the Target can be healed, False if not
+         */
         bool canHealTarget(Character &healer, Character &target);
 
+        /**
+         *
+         * @param combat state to check in active_combats
+         * @return Returns True if the Combat param exists in active_combats, False if not
+         */
         bool canAttackTarget(const CombatState &combat);
 
+        // TODO: Complete implementation detail
         bool canFlee(const CombatState &combat);
     };
 };
