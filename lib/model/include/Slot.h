b2851c76e3104c8cde595684502511777eb5e5e0
Junho <junhok@sfu.ca>
2019-03-19 12:21:46 -0700

    Create Slot file

---
+++ lib/model/include/Slot.h
@@ -0,0 +1,18 @@
+//
+// Created by jnhkm on 2019-03-19.
+//
+
+#ifndef SLOT_H
+#define SLOT_H
+
+
+namespace model {
+    // Count returns the length of the Slots
+    enum Slot{
+        Head, Shoulders, Back, Chest, Hands, Legs, Feet, Weapon, Misc, Count
+    };
+
+}
+
+
+#endif //SLOT_H
---
+++ lib/model/src/Slot.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jnhkm on 2019-03-19.
+//
+
+#include "Slot.h"
