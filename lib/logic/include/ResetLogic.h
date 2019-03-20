27434fca7a5e6877f20d185e803563ebe6f91685
arehal <arehal@sfu.ca>
2019-03-19 22:19:49 -0700

    created ResetLogic class

---
+++ lib/logic/include/ResetLogic.h
@@ -0,0 +1,26 @@
+//
+// Created by ARehal on 2019-03-19.
+//
+
+#ifndef RESETLOGIC_H
+#define RESETLOGIC_H
+
+#include "Reset.h"
+#include "Room.h"
+
+using model::Reset;
+using model::Room;
+
+namespace logic {
+    class ResetLogic {
+    private:
+    public:
+        ResetLogic();
+
+        bool canAddNpcToRoom(const Reset reset, const Room room) const;
+        bool canAddObjectToRoom(const Reset reset, const Room room) const;
+    };
+
+}
+
+#endif //RESETLOGIC_H
